a970b47ef79fb309e20a1f371a696f0d451bdb7e
nimabarani <brn.nima@gmail.com>
2023-02-14 23:22:33 -0500

    Finished: bowtie_align.sh

--- bowtie_align.sh
+++ bowtie_align.sh
@@ -6,10 +6,9 @@
 
 module load bowtie2/2.4.4
 
-srid=$1
-genomeDir="../genomes/pathogens/sl1344"
-readDir="../raw_reads/$srid"
+srid="SRR10005567"
+genomeDir="../genomes/pathogens/STM"
+readDir="/home/nima/scratch/dual_rna/raw_reads/experiment3/infection/$srid"
 
-
-# bowtie2 -p 4 --very-sensitive -x $genomeDir/index/pathogen -q -1 $readDir/${readDir}_paired_1.fastq -2 ${readDir}_paired_2.fastq -S $genomeDir/output.sam
-bowtie2 -p 4 --very-sensitive -x $genomeDir/index/pathogen -q $readDir/${srid}_trimmed.fastq -S $readDir/pathogen_align.sam
+bowtie2 -p 4 --very-sensitive -x $genomeDir/index/pathogen -q -1 $readDir/${srid}_paired_1.fastq -2 $readDir/${srid}_paired_2.fastq -S $genomeDir/output.sam
+# bowtie2 -p 4 --very-sensitive -x $genomeDir/index/pathogen -q $readDir/${srid}_trimmed.fastq -S $readDir/pathogen_align.sam
