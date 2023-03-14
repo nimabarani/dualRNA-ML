aa4f975c4bec7b381281f22c0c00cf96ad828b97
nimabarani <brn.nima@gmail.com>
2023-03-14 00:50:30 -0400

    Finish sambamba.sh

--- sambamba.sh
+++ sambamba.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --time=10:30:00
+#SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=32GB
 #SBATCH --output='../logs/sambamba-%A.out'
 
 module load sambamba/0.8.0
 
+for sam_dir in /home/nima/scratch/dual_rna/raw_reads/experiment*/{infection,pathogen}/*/*.sam
+do
+    sra_dir=$(dirname $sam_dir)
 
-samDir="/home/nima/scratch/dual_rna/raw_reads/experiment2/host/SRR15827083/star"
+    sambamba view -t 18 -S $sra_dir/pathogen_align.sam -f bam -o $sra_dir/proband_tmp.bam
+    sambamba sort -t 18 -n $sra_dir/proband_tmp.bam -o $sra_dir/output_sorted_name.bam
 
-sambamba sort -t 12 -n $samDir/star_Aligned.sortedByCoord.out.bam -o $samDir/output_sorted_name.bam 
+done 
+# sambamba view -t 12 -S $sam_dir/pathogen_align.sam -f bam -o $sam_dir/proband_tmp.bam
+# sambamba sort -t 12 -n $sam_dir/proband_tmp.bam -o $sam_dir/output_sorted_name.bam
+# done
