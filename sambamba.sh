327dcf25f8251d390183fd415dab8410f496ab7d
nimabarani <brn.nima@gmail.com>
2023-03-15 01:30:31 -0400

    Make iterable sambamba.sh

--- sambamba.sh
+++ sambamba.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --time=10:30:00
+#SBATCH --time=05:00:00
 #SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=32GB
 #SBATCH --output='../logs/sambamba-%A.out'
 
 module load sambamba/0.8.0
 
-for sam_dir in /home/nima/scratch/dual_rna/raw_reads/experiment*/{infection,pathogen}/*/*.sam
+for sam_dir in /home/nima/scratch/dual_rna/raw_reads/experiment*/{infection,host}/*/star
 do
-    sra_dir=$(dirname $sam_dir)
-
-    sambamba view -t 18 -S $sra_dir/pathogen_align.sam -f bam -o $sra_dir/proband_tmp.bam
-    sambamba sort -t 18 -n $sra_dir/proband_tmp.bam -o $sra_dir/output_sorted_name.bam
-
+    if [[ ! -f $sam_dir/output_sorted_name.bam ]]
+    then
+        echo $sam_dir
+        sambamba sort -t 18 -n $sam_dir/star_Aligned.sortedByCoord.out.bam -o $sam_dir/output_sorted_name.bam
+    fi
 done 
 # sambamba view -t 12 -S $sam_dir/pathogen_align.sam -f bam -o $sam_dir/proband_tmp.bam
 # sambamba sort -t 12 -n $sam_dir/proband_tmp.bam -o $sam_dir/output_sorted_name.bam
