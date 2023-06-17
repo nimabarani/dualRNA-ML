605e350b033fbf1b1a7e86ed688296bdf22daf84
nimabarani <brn.nima@gmail.com>
2023-06-16 20:52:17 -0400

    Refactor code to modular functions to improve clarity on specific processes.
    - Modularized the process of preparing data, selecting features, and training the model
    - Removed redundant comment and rearranged the script for better readability.

--- train.py
+++ train.py
@@ -23,41 +23,42 @@ from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 
 
-def prepare_data(features_path, data_path):
-    selected_features = pd.read_csv(features_path)['feature'].to_numpy()
-
-    df = pd.read_pickle(data_path).reset_index(level='label')
+def get_train_data(path):
+    df: pd.DataFrame = pd.read_pickle(path)
+    df = df.droplevel("nameseq").reset_index()
     df = df.sample(frac=1)
+
     X_train = df.iloc[:, 1:]
     X_train.columns = range(X_train.shape[1])
-    X_train = X_train[selected_features]
-    y_train = df.iloc[:, 0]
-    y_train = y_train.map({'nd': 0, 'up': 1, 'down': 2})
-
+    y_train = df.iloc[:, 0].map({"nd": 0, "up": 1, "down": 2})
     return X_train, y_train
 
 
-def train_model(X_train, y_train):
-    skf = StratifiedKFold(
-        n_splits=10,
-    )
+def get_selected_features(path, X):
+    features_df = pd.read_csv(path)
+    selected_features = features_df[features_df["counts"] >= 30]["feature"]
+    return X[selected_features]
+
+
+def train_models(X, y):
+    skf = StratifiedKFold(n_splits=10)
 
     scorings = {
-        'acc': 'accuracy',
-        'f1_macro': 'f1_macro',
-        'roc_macro': 'roc_auc_ovr',
+        "acc": "accuracy",
+        "f1_macro": "f1_macro",
+        "roc_macro": "roc_auc_ovr",
     }
 
-    scalers = [
-        None,
-        StandardScaler(),
-        MinMaxScaler(),
-        RobustScaler(),
-        MaxAbsScaler(),
-        QuantileTransformer(output_distribution='normal'),
-        QuantileTransformer(output_distribution='uniform'),
-        PowerTransformer(),
-    ]
+    # scalers = [
+    #     None,
+    #     StandardScaler(),
+    #     MinMaxScaler(),
+    #     RobustScaler(),
+    #     MaxAbsScaler(),
+    #     QuantileTransformer(output_distribution='normal'),
+    #     QuantileTransformer(output_distribution='uniform'),
+    #     PowerTransformer(),
+    # ]
 
     classifiers = [
         KNeighborsClassifier(),
@@ -69,49 +70,29 @@ def train_model(X_train, y_train):
         XGBClassifier(),
         LGBMClassifier(),
     ]
-    results = {}
 
     for classifier in classifiers:
         classifier_name = type(classifier).__name__
-        scaler_results = {}
-        for scaler in scalers:
-            scaler_name = type(scaler).__name__
-            model = make_pipeline(scaler, classifier)
-            cv_results = cross_validate(
-                model, X_train, y_train, cv=skf, scoring=scorings, n_jobs=-1
-            )
-            print(
-                f"{classifier_name}\t{scaler_name}\t{cv_results['test_roc_macro'].mean():.4f}"
-            )
-            scaler_results[scaler_name] = cv_results
-        results[classifier_name] = scaler_results
-        print('-' * 50)
-    return results
+        cv_results = cross_validate(
+            classifier, X, y, cv=skf, scoring=scorings, n_jobs=-1
+        )
+        print(f"{classifier_name}\t{cv_results['test_roc_macro'].mean():.4f}")
+        print("-" * 50)
 
 
-def main(data_path, features_path, output_path):
-    X_train, y_train = prepare_data(
-        data_path=data_path, features_path=features_path
-    )
-    results = train_model(X_train=X_train, y_train=y_train)
-    with open(output_path, 'wb') as file:
-        pickle.dump(results, file)
+def main(data_path, features_path):
+    X_train, y_train = get_train_data(data_path)
+    selected_X_train = get_selected_features(features_path, X_train)
+    train_models(X_train, y_train)
+    print("SELECTED FEATURES")
+    train_models(selected_X_train, y_train)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Train different scalers and models'
-    )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train different scalers and models")
     # Add arguments
-    parser.add_argument(
-        '-i', '--input', required=True, help='Input Pickle file'
-    )
-    parser.add_argument(
-        '-f', '--features', required=True, help='Selected features file'
-    )
-    parser.add_argument(
-        '-o', '--output', required=True, help='Output report path'
-    )
+    parser.add_argument("-i", "--input", required=True, help="Input Pickle")
+    parser.add_argument("-f", "--features", required=True, help="Selected features")
 
     # Parse the arguments
     args = parser.parse_args()
@@ -119,6 +100,5 @@ if __name__ == '__main__':
     # # Access the arguments
     data_path = args.input
     features_path = args.features
-    output_path = args.output
 
-    main(data_path, features_path, output_path)
+    main(data_path, features_path)
