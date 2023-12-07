eecdb5c811ed02b5cb8ad3aeff0bd940bcab64aa
nimabarani <brn.nima@gmail.com>
2023-12-06 22:59:45 -0500

    Replace GridSearchCV with HyperOpt

--- grid_search.py
+++ grid_search.py
@@ -1,60 +1,77 @@
 import argparse
+import numpy as np
 import pandas as pd
-from lightgbm import LGBMClassifier
-from mrmr import mrmr_classif
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from hyperopt import Trials, STATUS_OK, tpe, fmin, STATUS_FAIL
+from sklearn.model_selection import StratifiedKFold, cross_validate
+import hyperparameters
+from sklearn.decomposition import PCA
+from mrmr_transformer import MRMRTransformer
+from sklearn.preprocessing import StandardScaler
+
+def get_data(path):
+    df = pd.read_pickle(path)
+    df.drop(columns="gene_id", inplace=True)
+    df.columns = np.arange(df.shape[1])
+    df = df.sample(frac=1, random_state=42, ignore_index=True)
+
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1].map({"nd": 0, "up": 1, "down": 2})
+    return X, y
 
 
-def get_train_data(path):
-    df: pd.DataFrame = pd.read_pickle(path)
-    df = df.droplevel("nameseq").reset_index()
-    df = df.sample(frac=1)
+def objective(params):
 
-    X_train = df.iloc[:, 1:]
-    X_train.columns = range(X_train.shape[1])
-    y_train = df.iloc[:, 0].map({"nd": 0, "up": 1, "down": 2})
-    return X_train, y_train
-
-
-def get_selected_features(X, y):
-    selected_features = mrmr_classif(X=X, y=y, K=200, n_jobs=-1)
-    return X[selected_features]
+    model = params["model"](**params["params"])
+    try:
+        cv_results = cross_validate(model, X, y, cv=skf, scoring="f1_macro", n_jobs=-1)
+        score = cv_results["test_score"].mean()
+        return {"loss": -score, "status": STATUS_OK}
+    except:
+        return {"status": STATUS_FAIL}
 
 
-def train_models(X, y):
+def main(data_path, model, feature, nfeature):
+    print(f"{model} - {feature} {nfeature}")
+    global X, y, skf, best_score
+    best_score = 0
+    X, y = get_data(data_path)
     skf = StratifiedKFold(n_splits=10)
-    parameter_grid = {
-        "boosting_type": ["gbdt", "dart", "goss"],
-        "num_leaves": [31, 63, 127],
-        "learning_rate": [0.1, 0.01, 0.001],
-        "n_estimators": [100, 200, 300],
-        "min_child_samples": [20, 50, 100],
-        "max_depth": [-1, 5, 10],
-    }
+    trials = Trials()
+    # log_file = f"{model}-{feature}.txt"
+    if model == "lgbm":
+        space = hyperparameters.lgbm_space
+    elif model == "rf":
+        space = hyperparameters.rf_space
+    elif model == "xgboost":
+        space = hyperparameters.xgb_space
 
-    clf = LGBMClassifier(random_state=42)
-    grid_search = GridSearchCV(
-        clf, parameter_grid, cv=skf, n_jobs=-1, scoring="roc_auc_ovr"
-    )
-    grid_search.fit(X, y)
+    # Feature Selection
+    if feature == "pca":
+        std_scl = StandardScaler()
+        X = std_scl.fit_transform(X, y)
+        pca = PCA(n_components=nfeature, random_state=42)
+        X = pca.fit_transform(X, y)
+    elif feature == "mrmr":
+        mrmr = MRMRTransformer(n_features=nfeature)
+        X = mrmr.fit_transform(X, y)
 
-    print(grid_search.best_score_)
-    print(grid_search.best_params_)
+    best = fmin(objective, space, algo=tpe.suggest, max_evals=1500, trials=trials)
 
-
-def main(data_path):
-    X_train, y_train = get_train_data(data_path)
-    selected_X_train = get_selected_features(X_train, y_train)
-    train_models(selected_X_train, y_train)
+    print(best)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train different scalers and models")
+    parser = argparse.ArgumentParser(description="Hyperparameter Tuning using HyperOpt")
     # Add arguments
     parser.add_argument("-i", "--input", required=True, help="Input Pickle")
+    parser.add_argument("-m", "--model", required=True, help="Model")
+    parser.add_argument("-f", "--feature", help="Feature selection method")
+    parser.add_argument("-n", "--nfeature", required=True, help="Number of features")
 
     # Parse the arguments
     args = parser.parse_args()
     data_path = args.input
-
-    main(data_path)
+    model = args.model
+    feature = args.feature
+    nfeature = int(args.nfeature)
+    main(data_path, model, feature, nfeature)
